ffebd4e90afd0843ba733c3700ac9c34a7dc02d7
Emile Nijssen <emile@athom.nl>
2024-08-25 11:53:21 +0200

    rm main

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /app
 
 COPY ./bin/ /app/bin/
 COPY ./lib/ /app/lib/
-COPY ./index.mjs /app/
 COPY ./package.json /app/
 COPY ./package-lock.json /app/
 
--- index.mjs
+++
@@ -1,7 +0,0 @@
-import GoogleBackup from './lib/GoogleBackup.mjs';
-
-const googleBackup = new GoogleBackup({
-  username: process.env.GOOGLE_USERNAME,
-  password: process.env.GOOGLE_PASSWORD,
-});
-await googleBackup.backupAll();
